e5034bd48d03a714cdda123c88e217b5a785898c
JulianAlexander <julian.pineyro@gmail.com>
2019-07-31 00:18:38 +0300

    Implemented Firebase for registration with validation +  material UI

--- src/components/Auth/Register.component.jsx
+++ src/components/Auth/Register.component.jsx
@@ -1,8 +1,261 @@
 import React from 'react';
+import {
+  Grid,
+  TextField,
+  Button,
+  IconButton,
+  InputAdornment,
+  SnackbarContent
+} from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
+import {
+  Polymer,
+  AccountCircle,
+  Lock,
+  ErrorRounded,
+  Email,
+  Visibility,
+  VisibilityOff
+} from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import firebase from '../../firebase';
+
+import './Register.styles.scss';
 
 class Register extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    showPassword: false,
+    errors: []
+  };
+
+  isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (this.isFormEmpty(this.state)) {
+      error = { message: 'All fields required!' };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: 'Invalid password' };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleClickShowPassword = () => {
+    this.setState({ showPassword: !this.state.showPassword });
+  };
+
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+  handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.isFormValid()) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  };
+
   render() {
-    return <div>Register</div>;
+    const {
+      username,
+      email,
+      password,
+      passwordConfirmation,
+      showPassword,
+      errors
+    } = this.state;
+
+    return (
+      <Grid
+        container
+        direction='column'
+        justify='center'
+        alignItems='center'
+        className='landing'
+      >
+        <form onSubmit={this.handleSubmit}>
+          <Grid
+            container
+            direction='column'
+            justify='center'
+            alignItems='center'
+            className='landing'
+          >
+            <Polymer
+              color='primary'
+              style={{ fontSize: 77 }}
+              className='register-logo'
+            />
+            <TextField
+              id='outlined-username'
+              label='Username'
+              name='username'
+              className=''
+              value={username}
+              onChange={this.handleChange}
+              margin='normal'
+              variant='outlined'
+              style={{ width: 233 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <AccountCircle />
+                  </InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              id='outlined-email'
+              label='Email'
+              name='email'
+              className=''
+              value={email}
+              onChange={this.handleChange}
+              margin='normal'
+              variant='outlined'
+              type='email'
+              style={{ width: 233 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <Email />
+                  </InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              id='outlined-password'
+              label='Password'
+              name='password'
+              className=''
+              value={password}
+              onChange={this.handleChange}
+              margin='normal'
+              variant='outlined'
+              type={showPassword ? 'text' : 'password'}
+              style={{ width: 233 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <Lock />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      aria-label='toggle password visibility'
+                      onClick={this.handleClickShowPassword}
+                      onMouseDown={this.handleMouseDownPassword}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              id='outlined-passwordConfirmation'
+              label='Password Confirmation'
+              name='passwordConfirmation'
+              className=''
+              value={passwordConfirmation}
+              onChange={this.handleChange}
+              margin='normal'
+              variant='outlined'
+              type={showPassword ? 'text' : 'password'}
+              style={{ width: 233 }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <Lock />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton
+                      aria-label='toggle password visibility'
+                      onClick={this.handleClickShowPassword}
+                      onMouseDown={this.handleMouseDownPassword}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+            <Button
+              type='submit'
+              size='large'
+              variant='contained'
+              color='primary'
+            >
+              Submit
+            </Button>
+            <div className='already-member'>
+              Already a Member?{' '}
+              <Link to='/login' className='login-link'>
+                Login
+              </Link>
+            </div>
+            {errors.length > 0 && (
+              <SnackbarContent
+                style={{ backgroundColor: red[700] }}
+                size='small'
+                message={
+                  <span className='error-message'>
+                    <ErrorRounded style={{ fontSize: 44 }} />
+                    {this.displayErrors(errors)}
+                  </span>
+                }
+              />
+            )}
+          </Grid>
+        </form>
+      </Grid>
+    );
   }
 }
 
